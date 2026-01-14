84eb07c0de5a8da1405aecaeca37b479ea959a7a
LeaFrock <petergeorge@126.com>
2026-01-15 00:13:30 +0800

    Add CaptchaN.Drawing.ImageMagick

---
+++ Src/CaptchaN.Drawing.ImageMagick/Fonts.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using ImageMagick;
+
+namespace CaptchaN.Drawing.ImageMagick;
+
+public static class Fonts
+{
+    public static IReadOnlyList<string> FontFamilies { get; set; } = [];
+
+    public static string RandomPick(Random rand)
+    {
+        var families = FontFamilies;
+        EnsureFontsLoaded(families);
+        var family = families[rand.Next(families.Count)];
+        return family;
+    }
+
+    public static void UseSystemFonts(Func<string, bool>? filter = default)
+    {
+        var families = filter is null
+            ? MagickNET.FontFamilies
+            : [.. MagickNET.FontFamilies.Where(filter)];
+        EnsureFontsLoaded(families);
+        FontFamilies = families;
+    }
+
+    public static void UseDirectoryFonts(DirectoryInfo directory)
+    {
+        if (directory.Exists)
+        {
+            var files = directory.GetFiles("*.ttf", SearchOption.TopDirectoryOnly);
+            if (files is { Length: > 0 })
+            {
+                List<string> families = new(files.Length);
+                foreach (var file in files)
+                {
+                    families.Add(file.FullName);
+                }
+                EnsureFontsLoaded(families);
+                FontFamilies = families;
+                return;
+            }
+        }
+        Throw();
+    }
+
+    private static void EnsureFontsLoaded(IReadOnlyList<string> families)
+    {
+        if (families.Count == 0)
+        {
+            Throw();
+        }
+    }
+
+    [DoesNotReturn]
+    private static void Throw() => throw new InvalidOperationException("No fonts loaded!");
+}
---
+++ Src/CaptchaN.Drawing.ImageMagick/ServiceCollectionExtension.cs
@@ -0,0 +1,26 @@
+﻿using CaptchaN.Abstractions;
+using CaptchaN.Drawing.ImageMagick;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtension
+{
+    public static void AddImageMagickPainter(this CaptchaNBuilder builder, IConfiguration configuration)
+    {
+        builder.Services.AddOptions<PainterOption>()
+            .Bind(configuration)
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+        builder.AddPainter(sp =>
+        {
+            var opt = sp.GetRequiredService<IOptions<PainterOption>>().Value;
+            if (!string.IsNullOrEmpty(opt.FontFolderPath))
+            {
+                // Fonts.UseDirectoryFonts(new(opt.FontFolderPath));
+            }
+            return new Painter(opt);
+        });
+    }
+}
